d24776544acc4e6d66d9fdb2ce92b574d26ac26e
navee <naveenarameshvardhini@gmail.com>
2021-12-23 14:43:53 +0530

    feat:Creating base structure

---
+++ lib/screens/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/screens/signup.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Signup extends StatelessWidget {
+  const Signup({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
