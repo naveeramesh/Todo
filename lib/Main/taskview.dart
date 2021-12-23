a52c2d35b944d0d57fdc71526cb04782e12bcd45
navee <naveenarameshvardhini@gmail.com>
2021-12-23 13:22:46 +0530

    fix:Simple Todo app

--- lib/Main/taskview.dart
+++ lib/Main/taskview.dart
@@ -17,62 +17,72 @@ class _TaskViewState extends State<TaskView> {
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance.collection("Task").snapshots(),
       builder: (context, snapshot) {
-        return ListView.builder(
-          itemCount: snapshot.data!.docs.length,
-          scrollDirection: Axis.vertical,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20, top: 10),
-              child: Container(
-                height: MediaQuery.of(context).size.height / 9,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Helper.text(snapshot.data!.docs[index]['taskname'],
-                              20, 0, Colors.black, FontWeight.bold),
-                          IconButton(
-                              onPressed: () {
-                                FirebaseFirestore.instance
-                                    .collection('Task')
-                                    .doc(snapshot.data!.docs[index]['time'])
-                                    .delete()
-                                    .whenComplete(() {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(
-                                        backgroundColor: appcolor,
-                                        content: Helper.text(
-                                            "You have completed your task",
-                                            15,
-                                            0,
-                                            Colors.white,
-                                            FontWeight.bold)),
-                                  );
-                                });
-                              },
-                              icon: Icon(Icons.cancel))
-                        ],
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator(
+            color: Colors.blue,
+          );
+        } else {
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20, top: 10),
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 9,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Helper.text(snapshot.data!.docs[index]['taskname'],
+                                20, 0, Colors.black, FontWeight.bold),
+                            IconButton(
+                                onPressed: () {
+                                  FirebaseFirestore.instance
+                                      .collection('Task')
+                                      .doc(snapshot.data!.docs[index]['time'])
+                                      .delete()
+                                      .whenComplete(() {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                          backgroundColor: appcolor,
+                                          content: Helper.text(
+                                              "You have completed your task",
+                                              15,
+                                              0,
+                                              Colors.white,
+                                              FontWeight.bold)),
+                                    );
+                                  });
+                                },
+                                icon: Icon(Icons.cancel))
+                          ],
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0, bottom: 8),
-                      child: Helper.text(snapshot.data!.docs[index]['schedule'],
-                          20, 0, Colors.grey, FontWeight.bold),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0, bottom: 8),
+                        child: Helper.text(
+                            snapshot.data!.docs[index]['schedule'],
+                            20,
+                            0,
+                            Colors.grey,
+                            FontWeight.bold),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
-        );
+              );
+            },
+          );
+        }
       },
     );
   }
